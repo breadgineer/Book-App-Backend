c42bd2aa4fb1884b54681299787344b028d58842
gerrigom <giancarlo.errigo-mattos@t-systems.com>
2021-12-10 16:08:42 +0100

    Added CORS

--- src/main/java/com/bookkapp/backend/BookAppBackendApplication.java
+++ src/main/java/com/bookkapp/backend/BookAppBackendApplication.java
@@ -2,6 +2,22 @@ package com.bookkapp.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+@EnableWebMvc
+class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
+
 
 @SpringBootApplication
 public class BookAppBackendApplication {
